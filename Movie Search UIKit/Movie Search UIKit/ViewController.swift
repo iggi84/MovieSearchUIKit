72153c577f1ffaeb4943ea249cb939875ad0f44f
Igor Vojinovic <igor.vojinovic@optmia-apps.com>
2023-05-27 18:00:24 +0200

    Initial commit

---
+++ Movie Search UIKit/Movie Search UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Search UIKit
+//
+//  Created by Igor  Vojinovic on 27.5.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
